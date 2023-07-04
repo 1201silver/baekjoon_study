6778ce93e453f9e13255510035d4da4c17be2c18
진고은 <1201silver@naver.com>
2023-07-04 17:16:49 +0900

    2-5

---
+++ baekjoon/src/step2/b2884.java
@@ -0,0 +1,29 @@
+package step2;
+
+import java.util.Scanner;
+
+public class b2884 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int h, m;
+		h = sc.nextInt();
+		m = sc.nextInt();
+
+		if(m < 45) {
+			if (h == 0) {
+				h = 23;
+			} else {
+				h -= 1;
+			}
+			m = 60 + (m - 45);
+		} else {
+			m -= 45;
+		}
+		
+		System.out.println(h +" "+ m);
+	}
+
+}
