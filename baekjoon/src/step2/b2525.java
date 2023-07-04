a34a9be84b93248e6292fb143e0f9104421c4f30
진고은 <1201silver@naver.com>
2023-07-04 17:17:04 +0900

    2-6

---
+++ baekjoon/src/step2/b2525.java
@@ -0,0 +1,30 @@
+package step2;
+
+import java.util.Scanner;
+
+public class b2525 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, c;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		if(b + c >= 60) {
+			a += ((b + c) / 60);
+			if(a >= 24) {
+				a -= 24;
+			} 
+			
+			b = (b + c) % 60;
+		} else {
+			b += c;
+		}
+
+		System.out.println(a +" "+ b);
+	}
+
+}
