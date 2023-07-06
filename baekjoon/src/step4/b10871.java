2c155726666dc7db6c74e0d9eeec450d2f057715
진고은 <1201silver@naver.com>
2023-07-06 18:31:52 +0900

    4-2

---
+++ baekjoon/src/step4/b10871.java
@@ -0,0 +1,44 @@
+package step4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class b10871 {
+
+	public static void main(String[] args) throws IOException {
+		/*
+		Scanner sc = new Scanner(System.in);
+		
+		int n, x;
+		n = sc.nextInt();
+		x = sc.nextInt();
+
+		for(int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			if(a < x) System.out.print(a +" ");
+		}
+		*/
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < n; i++) {
+			int a = Integer.parseInt(st.nextToken());
+			if(a < x) bw.write(a +" ");
+		}
+		bw.close();
+	}
+
+}
