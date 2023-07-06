79ad75afd280fc90cfa36123c668f0aba2f3fe2b
진고은 <1201silver@naver.com>
2023-07-06 21:05:31 +0900

    4-7

---
+++ baekjoon/src/step4/b5597.java
@@ -0,0 +1,35 @@
+package step4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class b5597 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int[] std = new int[30];
+		
+		for(int i = 0; i < 30; i++) {
+			std[i] = 0;
+		}
+		
+		for(int i = 0; i < 28; i++) {
+			int num = Integer.parseInt(br.readLine());
+			std[num - 1] = num;
+		}
+
+		for(int i = 0; i < 30; i++) {
+			if(std[i] == 0) bw.write((i + 1) +"\n");
+//			bw.write(std[i] +"\n");
+		}
+		
+		bw.close();
+	}
+
+}
