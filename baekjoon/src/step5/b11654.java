8b1f1f926b66056e1bd1566a82e7a572cdcedae4
진고은 <1201silver@naver.com>
2023-07-08 22:42:14 +0900

    5-4

---
+++ baekjoon/src/step5/b11654.java
@@ -0,0 +1,18 @@
+package step5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b11654 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String word = br.readLine();
+		System.out.println((int)word.charAt(0));
+
+	}
+
+}
