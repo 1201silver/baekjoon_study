9f111005f5df0955e94f0b7b08c98219e3ccb8f1
진고은 <1201silver@naver.com>
2023-07-09 21:23:39 +0900

    5-8

---
+++ baekjoon/src/step5/b1152.java
@@ -0,0 +1,24 @@
+package step5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b1152 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		String[] word = br.readLine().split(" ");
+		
+		int cnt = 0;
+		for(int i = 0; i < word.length; i++) {
+			if(word[i].length() == 0) cnt++;
+		}
+		System.out.println(word.length - cnt);
+	}
+
+}
