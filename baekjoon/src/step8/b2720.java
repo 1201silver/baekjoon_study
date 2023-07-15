d2035fe086cbfa033ede11bbb45ffde9c6de8e52
진고은 <1201silver@naver.com>
2023-07-15 22:56:06 +0900

    8-3

---
+++ baekjoon/src/step8/b2720.java
@@ -0,0 +1,36 @@
+package step8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b2720 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		
+		int [][] change = new int[n][4];
+		
+		for(int i = 0; i < n; i++) {
+			int cent = Integer.parseInt(br.readLine());
+			
+			int q = cent / 25;
+			cent %= 25;
+			
+			int d = cent / 10;
+			cent %= 10;
+			
+			int ni = cent / 5;
+			cent %= 5;
+			
+			int p = cent / 1;
+			cent %= 11;
+			
+			System.out.println(q +" "+ d +" "+ ni +" "+ p);
+		}
+	}
+
+}
