14a7f1c5622b1047456a3735cdc93044d30f0086
진고은 <1201silver@naver.com>
2023-07-18 22:23:04 +0900

    10-5

---
+++ baekjoon/src/step10/b9063.java
@@ -0,0 +1,39 @@
+package step10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b9063 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+
+		int x_min = Integer.MAX_VALUE;
+		int x_max = Integer.MIN_VALUE;
+		
+		int y_min = Integer.MAX_VALUE;
+		int y_max = Integer.MIN_VALUE;
+		
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			if(x < x_min) x_min = x;
+			if(x > x_max) x_max = x;
+			
+			if(y < y_min) y_min = y;
+			if(y > y_max) y_max = y;
+		}
+		
+		System.out.println((x_max - x_min) * (y_max - y_min));
+		
+	}
+
+}
