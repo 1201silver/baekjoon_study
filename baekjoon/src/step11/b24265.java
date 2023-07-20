c323aa630fafb5d964faeef34dff9bf5e8194042
진고은 <1201silver@naver.com>
2023-07-20 21:10:01 +0900

    11-4

---
+++ baekjoon/src/step11/b24265.java
@@ -0,0 +1,19 @@
+package step11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b24265 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		long n = Integer.parseInt(br.readLine());
+		
+		System.out.println((n * (n - 1)) / 2 +"\n"+ 2);
+
+	}
+
+}
