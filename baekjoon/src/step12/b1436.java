efbbe3e89bb867c97c1a8946d3e207bcb72179b4
진고은 <1201silver@naver.com>
2023-07-21 20:34:31 +0900

    12-5

---
+++ baekjoon/src/step12/b1436.java
@@ -0,0 +1,28 @@
+package step12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b1436 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int cnt = 0;
+		int i = 665;
+		while(true) {
+			if(String.valueOf(i).contains("666")) {
+				cnt ++;
+			}
+			if(cnt == n) break;
+			
+			i++;
+		}
+		System.out.println(i);
+	}
+
+}
