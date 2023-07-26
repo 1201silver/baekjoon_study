2ba59e50ac64ba4152b54b977199ecb2cde7c171
진고은 <1201silver@naver.com>
2023-07-26 18:09:21 +0900

    14-2

---
+++ baekjoon/src/step14/b14425.java
@@ -0,0 +1,33 @@
+package step14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class b14425 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		HashSet<String> word = new HashSet<String>();
+		for(int i = 0; i < n; i++) {
+			word.add(br.readLine());
+		}
+		
+		int cnt = 0;
+		for(int i = 0; i < m; i++) {
+			if(word.contains(br.readLine())) cnt++;
+		}
+		
+		System.out.println(cnt);
+	}
+
+}
