7764f6e3ce5a8293d05ce6b73c5665242cae8551
진고은 <1201silver@naver.com>
2023-07-27 21:06:16 +0900

    14-8

---
+++ baekjoon/src/step14/b11478.java
@@ -0,0 +1,30 @@
+package step14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class b11478 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+
+		HashSet<String> str = new HashSet<String>();
+		
+		int i = 0;
+		while((s.length() - i) > 0) {
+			for(int j = 0; j < s.length() - i; j++) {
+				str.add(s.substring(j, j+i+1));
+				
+			}
+			i++;
+		}
+		
+		System.out.println(str.size());
+	}
+
+}
